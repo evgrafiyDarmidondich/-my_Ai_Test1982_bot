9bc1ee1d9befa662792bbc1c54cca34d00716ccb
vaceslav <irbis19822801@gmail.com>
2025-07-19 15:13:04 +0400

    создал генератор ответов юзеру от ИИ и роутер вопроса

---
+++ app/generators.py
@@ -0,0 +1,25 @@
+import os
+
+from openai import AsyncOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+client = AsyncOpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=os.getenv('AI_API_KEY'),
+)
+
+
+async def ai_generate(req):
+    completion = await client.chat.completions.create(
+        extra_body={},
+        model="deepseek/deepseek-chat-v3-0324:free",
+        messages=[
+            {
+                "role": "user",
+                "content": req,
+                "temterature": "0.0"
+            }
+        ]
+      )
+    return completion.choices[0].message.content
--- app/user.py
+++ app/user.py
@@ -1,9 +1,12 @@
 import asyncio
+from tokenize import generate_tokens
 
 from aiogram import Router, F
 from aiogram.enums import ChatAction
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
+
+from app.generators import ai_generate
 
 user_router = Router()
 
@@ -13,3 +16,8 @@ async def cmd_star_user(message: Message):
     await asyncio.sleep(0.5)
     await message.answer('Привет')
 
+# Роутер реагирует на любой текст ответом от ИИ
+@user_router.message()
+async def user_message(message: Message):
+    response = await ai_generate(message.text)
+    await message.answer(response, parse_mode='Markdown')
